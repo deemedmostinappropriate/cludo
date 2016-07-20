e4399b9e5ab972c83f0208a7066b55d04ec2ece3
deemedmostinappropriate <hc.adoak@gmail.com>
2016-07-20 14:42:18 +1200

    Initial test
    Test of version control

---
+++ Game.java
@@ -0,0 +1,15 @@
+package assignment1.cluedo;
+
+public class Game {
+	static class Card{
+		public enum WEAPON{
+			ROPE, DAGGER, CANDLESTK, REVOLVER, LEADPIPE, SPANNER;
+		}
+		public enum ROOM{
+			HALL, DINING, BILLIARD, CONSERVATORY, BALL, LIBRARY, STUDY, LOUNGE, KITCHEN;
+		}
+		public enum CHAR{
+			SCARLET, MUSTARD, WHITE, PLUM, GREEN, PEACOCK;
+		}
+	}
+}
