047bfa95bfd71514fb07a4951c06b0d63676b13a
danaxe7 <danaxe7@hotmail.com>
2016-07-20 23:04:41 +1200

    Move method considerations

--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,39 @@
 
 public class Player {
 
+	Character character;
+	
+	public Player(Character ch){
+		this.character = ch;
+	}
+	
+	/**
+	 * Moves the player's piece on the board.
+	 * @param New x coordinate
+	 * @param New y coordinate
+	 * @param The game board.
+	 * @return Whether or not the player is able to make the move.
+	 */
+	public boolean move(int diceroll, int x, int y, Board board){
+		boolean result = false;
+		 //////MAJOR BREAkTHROUGH, move the player one step at a time. bypass most checks :D
+		
+		
+		//decides legality of move.
+		//Bad cases
+		// x = current x and y = current y.  new position is same as old.
+		////Blatant distance issue
+		// x - diceroll > current x
+		// x + diceroll < current x
+		// y - diceroll > current y
+		// y + diceroll < current y
+		
+		//total distance moved is less than diceroll, and new position is not in a room
+		
+		
+		//if endpoint < diceroll somehow, and it is not in a room.
+		
+		
+		return result;
+	}
 }
