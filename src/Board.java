ceb9189dd8eb98b2b464823d6fb5900935635a5f
danaxe7 <danaxe7@hotmail.com>
2016-07-25 16:47:17 +1200

    Method and field changes

--- src/Board.java
+++ src/Board.java
@@ -3,18 +3,22 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Board {
 	private int[][] board;		//filled with 1's where a traversable square is.
 	private List<Room> rooms;
-	private Door[][] doors;		// Door locations.
+	private Door[][] doors;				// Door locations.
+	private Set<Character> characters;	//A set of character pieces on the board.
 
 	public Board(){
 		this.board = new int[25][25];
 		this.rooms = new ArrayList<Room>();
 		this.doors = new Door[25][25];
+		this.characters = new HashSet<>();
 		//Adds rooms to the board.
 		this.rooms.add(new Room("LOUNGE"));
 		this.rooms.add(new Room("DINING_ROOM"));
@@ -38,6 +42,33 @@ public class Board {
 	public int[][] getBoard(){
 		return this.board;
 	}
+	
+	/**
+	 * Finds the room with a name that matches the argument.
+	 * @param The name to match;
+	 * @return The room which matches the name;
+	 */
+	public Room findRoom(String name){
+		for(Room room : this.rooms){
+			if(room.NAME.equals(name))
+				return room;
+		}
+		return null;
+	}
+	
+	/**
+	 * Finds the room with a name that matches the argument.
+	 * @param The name to match;
+	 * @return The character which matches the name;
+	 */
+	public Character findCharacter(String name){
+		for(Character character : this.characters){
+			if(character.NAME.equals(name))
+				return character;
+		}
+		return null;
+	}
+
 
 	/**
 	 * Reads the file ascii-map.txt to fill in the board array.
