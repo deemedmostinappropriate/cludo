66df10582227c53a1f30e25facee324554a94105
danaxe7 <danaxe7@hotmail.com>
2016-07-28 22:06:24 +1200

    Card class from enum

---
+++ src/Card.java
@@ -0,0 +1,33 @@
+/**
+ * Represents cards in the player's hands, and the cards in the solution.
+ * @author Daniel Anastasi
+ *
+ */
+public class Card{
+	/**
+	 * The six weapon cards.
+	 * @author Daniel Anastasi
+	 *
+	 */
+	public enum WEAPON{
+		ROPE, DAGGER, CANDLESTICK, REVOLVER, LEADPIPE, SPANNER;
+	}
+	
+	/**
+	 * The nine room cards.
+	 * @author Daniel Anastasi
+	 *
+	 */
+	public enum ROOM{
+		HALL, DINING, BILLIARD, CONSERVATORY, BALL, LIBRARY, STUDY, LOUNGE, KITCHEN;
+	}
+	
+	/**
+	 * The six character cards.
+	 * @author Daniel Anastasi
+	 *
+	 */
+	public enum CHAR{
+		SCARLET, MUSTARD, WHITE, PLUM, GREEN, PEACOCK;
+	}
+}
