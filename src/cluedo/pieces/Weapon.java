4ba3206d5e069444490e5d1262a64b8adb25dc4b
danaxe7 <danaxe7@hotmail.com>
2016-08-02 21:17:01 +1200

    Position and getters/setters

--- src/cluedo/pieces/Weapon.java
+++ src/cluedo/pieces/Weapon.java
@@ -11,4 +11,24 @@ public enum Weapon implements Piece{
 		REVOLVER,
 		ROPE,
 		SPANNER;
+	
+	private int x = 0, y = 0;
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+
 }
